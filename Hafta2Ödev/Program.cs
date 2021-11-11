4745b010f58dafedf65f3a9fa7f9fe9ab5229b12
super <super@LAPTOP-3LCJEP17>
2021-11-11 18:41:06 +0300

    Add project files.

---
+++ Hafta2Ödev/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Hafta2Ödev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int girisHakkı = 3;
+            while (true)
+            {
+                string kullaniciAdi, sifre;
+                Console.WriteLine("Kullanıcı adı giriniz...");
+                kullaniciAdi = Console.ReadLine();
+
+                Console.WriteLine("Şifre giriniz..");
+                sifre = Console.ReadLine();
+                if(kullaniciAdi=="admin"&&sifre=="1234")
+                {
+                    Console.WriteLine("Hoşgeldiniz...");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Kullanıcı adınız veya şifreniz hatalı!!!");
+                    if (girisHakkı>0)
+                    {
+                        girisHakkı -= 1; 
+                    }
+                    if (girisHakkı == 0)
+                    {
+                        Console.WriteLine("3 hatalı giriş yaptığınız için oturumunuz sonlandırılmıştır.");
+                        break;
+                    }
+                    }
+                }
+                 Console.ReadKey();
+            }
+        }     
+    }
+
---
+++ OrtalamaHesap/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace OrtalamaHesap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            double ortalama;
+            double enBuyuk = 0, enKucuk = 0;
+            double vizeToplam = 0, finalToplam = 0;
+            Console.WriteLine("Öğrenci sayısı giriniz");
+            int n = Convert.ToInt32(Console.ReadLine());
+            
+            for (int i= 0; i < n; i++)
+            {
+                Console.WriteLine($"{i+1}.öğrenci vize notunu giriniz");
+                int vizeNotu = Convert.ToInt32(Console.ReadLine());
+                vizeToplam += vizeNotu;
+
+                Console.WriteLine($"{i + 1}.öğrenci final notunu giriniz");
+                int finalNotu = Convert.ToInt32(Console.ReadLine());
+                finalToplam += finalNotu;
+               ortalama = ((vizeNotu * 0.4) + (finalNotu * 0.6));
+                if (i==0)
+                {
+                    enKucuk = ortalama;
+                    enBuyuk = ortalama;
+                }
+                if (enBuyuk < ortalama)
+                {
+                    enBuyuk = ortalama;
+                }
+                if(enKucuk>ortalama)
+                {
+                    enKucuk = ortalama;
+                        }
+            }
+            Double genelOrt = ((vizeToplam * 0.4) + (finalToplam * 0.6)) / n;
+            
+            Console.WriteLine("En yüksek not ortalaması="+enBuyuk);
+            Console.WriteLine("En düşük not ortalaması="+enKucuk);
+            Console.WriteLine("Genel ortalama=" + genelOrt);
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
